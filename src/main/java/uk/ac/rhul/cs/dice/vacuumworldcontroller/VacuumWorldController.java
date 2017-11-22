9645113fa2c9af5f5838f3afa4f1202ba63a62a3
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2017-11-22 15:51:24 +0000

    Minor.

--- src/main/java/uk/ac/rhul/cs/dice/vacuumworldcontroller/VacuumWorldController.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworldcontroller/VacuumWorldController.java
@@ -2,6 +2,8 @@ package uk.ac.rhul.cs.dice.vacuumworldcontroller;
 
 public class VacuumWorldController {
     
+    private VacuumWorldController() {}
+    
     public static void main(String[] args) {
         //TODO
     }
