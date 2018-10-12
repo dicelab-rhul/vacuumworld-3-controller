4693a00a791aeabe309a23687b763ea2f75d97c5
cloudstrife9999 <Emanuele.Uliana.2016@live.rhul.ac.uk>
2018-10-12 18:13:03 +0100

    Bugfix.

--- src/main/java/uk/ac/rhul/cs/dice/vacuumworldcontroller/Main.java
+++ src/main/java/uk/ac/rhul/cs/dice/vacuumworldcontroller/Main.java
@@ -8,32 +8,34 @@ import org.cloudstrife9999.logutilities.LogUtils;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import uk.ac.rhul.cs.dice.vacuumworld.vwcommon.VWJSON;
+
 public class Main {
-    private static final String HOST = "127.0.0.1";
     private static final String CONFIG_FILE_PATH = "config.json";
     
     private Main() {}
     
     public static void main(String[] args) throws IOException {
+	LogUtils.enableVerbose();
 	LogUtils.log("Controller started.");
 	
-	String[] ports = getPorts();
+	String[] details = getDetails();
 	
-	if (!checkPorts(ports[0], ports[1])) {
+	if (!checkPorts(details[0], details[1])) {
 	    LogUtils.log("Malformed or illegal details have been provided. Please edit " + CONFIG_FILE_PATH + " and retry.");
 	}
 	else {
-	    VacuumWorldController controller = new VacuumWorldController(Integer.valueOf(ports[0]), HOST, Integer.valueOf(ports[1]));
+	    VacuumWorldController controller = new VacuumWorldController(Integer.valueOf(details[0]), details[2], Integer.valueOf(details[1]));
 	    controller.init();
 	}
     }
     
-    private static String[] getPorts() {
+    private static String[] getDetails() {
 	try {
 	    JSONTokener tokener = new JSONTokener(new FileInputStream(CONFIG_FILE_PATH));
 	    JSONObject root = new JSONObject(tokener);
 	    
-	    return new String[] {root.getString("controller_port"), root.getString("model_port")};
+	    return new String[] {root.getString(VWJSON.CONTROLLER_PORT), root.getString(VWJSON.MODEL_PORT), root.getString(VWJSON.MODEL_HOSTNAME)};
 	}
 	catch(FileNotFoundException e) {
 	    LogUtils.fakeLog(e);
